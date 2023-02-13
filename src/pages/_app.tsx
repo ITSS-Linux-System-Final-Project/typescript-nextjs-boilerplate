d98060e630b4260d67826c1c210e6f456e93024b
Francisco Minh Dang <dangpm@paditech.com>
2023-02-13 15:10:08 +0700

    fix(Fix lint): Change import antd

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import '@/styles/globals.scss';
-import 'antd/dist/antd.css';
+import 'antd/dist/reset.css';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
